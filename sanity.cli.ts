6962df10f05513bb58cb47ecd65778fd8a71cd76
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-06 13:16:17 +0800

    ref: added studioHost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'qyt42r3p',
-    dataset: 'development'
+    dataset: 'development',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
+  studioHost: 'its-hub',
   autoUpdates: true,
 })
