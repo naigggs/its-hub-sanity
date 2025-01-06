375af6e54162b58107673a0f05e4975fbd8b48dc
Alaics <acerlaptop0619@gmail.com>
2025-01-06 11:28:44 +0800

    feat: added blog

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import blockContent from './blockContent'
+import blog from './blog'
 import home from './home'
 
-export const schemaTypes = [blockContent, home]
+export const schemaTypes = [blockContent, home, blog]
