0e2d9e8f3c570b39e8133063f463621e225085e4
Gian Cabigting <gdcabigting.dev@gmail.com>
2025-01-06 11:32:59 +0800

    fix: fix error

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import blockContent from './blockContent'
 import blog from './blog'
 import home from './home'
-
-export const schemaTypes = [blockContent, home, blog]
-
-export const schemaTypes = [blockContent, home, careers]
 import careers from './careers'
+
+
+export const schemaTypes = [blockContent, home, careers, blog]
+
